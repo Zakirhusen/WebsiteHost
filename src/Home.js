268ac130827d42e6aa45a7cd330d75feaed327f9
Zakirhusen <72880935+Zakirhusen@users.noreply.github.com>
2021-09-09 11:56:01 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <CommonHA imgSrc='https://media.istockphoto.com/vectors/business-character-analysing-investment-strategies-and-portfolio-of-vector-id1324639498?b=1&k=6&m=1324639498&s=170667a&w=0&h=5fMMMlFJSwOYtIYrymCx0wqmRnNO6CCtXH_FVBh-BKc=' btnTitle='Get Started'
-        name="Grow your business with " visit='/services'
+        name="Grow your business with jjjjj " visit='/services'
       />
     </>
   );
